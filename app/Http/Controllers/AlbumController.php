5b7a155e332ba2170ac1a4d493d99503810a6384
AdetonaMichael <powerofelectron616@gmail.com>
2023-02-01 12:30:58 +0100

    created grouping and prifix for api routes

---
+++ app/Http/Controllers/AlbumController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Album;
+use App\Http\Requests\StoreAlbumRequest;
+use App\Http\Requests\UpdateAlbumRequest;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Album::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAlbumRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAlbumRequest $request)
+    {
+       $album = Album::create($request->all());
+       return $album;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Album $album)
+    {
+         return $album;
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAlbumRequest  $request
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAlbumRequest $request, Album $album)
+    {
+        $album->update($request->all());
+        return $album;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Album $album)
+    {
+        $album->delete('Album Deleted Successfully...!', 204);
+    }
+}
---
+++ database/migrations/2023_01_31_230437_create_image_manipulations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Album;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_manipulations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('path', 2000);
+            $table->string('type', 25);
+            $table->text('data');
+            $table->string('output_path', 2000)->nullable();
+            $table->timestamps();
+            $table->foreignIdFor(User::class, 'user_id')->nullable();
+            $table->foreignIdFor(Album::class, 'album_id')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_manipulations');
+    }
+};
