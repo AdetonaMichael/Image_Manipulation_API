d1ebfa76c846dd079737310edc1325cf1ab67f8e
AdetonaMichael <powerofelectron616@gmail.com>
2023-02-02 11:48:07 +0100

    created the api resources for Album

---
+++ app/Http/Controllers/V1/ImageManipulationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\ImageManipulation;
+use App\Http\Requests\StoreImageManipulationRequest;
+use App\Http\Requests\UpdateImageManipulationRequest;
+
+class ImageManipulationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreImageManipulationRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreImageManipulationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ImageManipulation  $imageManipulation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ImageManipulation $imageManipulation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateImageManipulationRequest  $request
+     * @param  \App\Models\ImageManipulation  $imageManipulation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateImageManipulationRequest $request, ImageManipulation $imageManipulation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ImageManipulation  $imageManipulation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ImageManipulation $imageManipulation)
+    {
+        //
+    }
+}
